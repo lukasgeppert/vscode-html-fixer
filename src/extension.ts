d36d02538d30913fb55cdbe81d4fe2875418f7cd
Lukas Geppert <elrg567@gmail.com>
2023-07-21 10:19:08 -0400

    refactor: Organize extension logic into a class and improve error handling
    This commit encapsulates the logic of the extension into an Extension class and adds error handling for HTML parsing with Cheerio. This refactor not only provides better organization of the code but also allows for more robust operation by catching potential HTML parsing errors, thus preventing application crashes. Additionally, user feedback is improved by adding success and error messages for the text replacement operation.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,50 @@
 import * as vscode from 'vscode';
 import * as cheerio from 'cheerio';
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.htmlFixer', function () {
-        let editor = vscode.window.activeTextEditor;
-        if (editor) {
-            let document = editor.document;
-            let text = document.getText();
+export class Extension {
+  constructor(private readonly context: vscode.ExtensionContext) {}
 
-            let $ = cheerio.load(text);
-            let fixedHtml = $.html();
+  activate() {
+    let disposable = vscode.commands.registerCommand('extension.htmlFixer', this.htmlFixer);
 
-            editor.edit(editBuilder => {
-                let firstLine = document.lineAt(0);
-                let lastLine = document.lineAt(document.lineCount - 1);
-                let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-                
-                editBuilder.replace(textRange, fixedHtml);
-            });
-        }
+    this.context.subscriptions.push(disposable);
+  }
+
+  htmlFixer() {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    let document = editor.document;
+    let text = document.getText();
+
+    let $;
+    let fixedHtml: string;
+    try {
+      $ = cheerio.load(text);
+      fixedHtml = $.html();
+    } catch (error) {
+      console.error('Error parsing HTML:', error);
+      vscode.window.showErrorMessage('Failed to parse HTML.');
+      return;
+    }
+
+    editor.edit(editBuilder => {
+      let firstLine = document.lineAt(0);
+      let lastLine = document.lineAt(document.lineCount - 1);
+      let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+
+      // Replace the text in the editor.
+      editBuilder.replace(textRange, fixedHtml);
+
+      // Check if the text was successfully replaced.
+      let originalText = document.getText(textRange);
+      if (originalText !== fixedHtml) {
+        vscode.window.showInformationMessage('HTML fixing complete!');
+      } else {
+        vscode.window.showErrorMessage('Failed to apply edit.');
+      }
     });
-
-    context.subscriptions.push(disposable);
+  }
 }
-
-
-
-export function deactivate() {}
