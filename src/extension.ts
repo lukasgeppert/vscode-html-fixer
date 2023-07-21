5da7558373b0f753f3d02c847e51aa74f2b89d46
Lukas Geppert <elrg567@gmail.com>
2023-07-21 16:16:37 -0400

    refactor: Revert Extension class to standalone functions
    This commit reverts the previous encapsulation of the vscode extension logic in the Extension class back to standalone activate() and deactivate() functions. The Extension class was found to be unnecessary and the code has been streamlined for readability and simplicity.

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,46 @@
 import * as vscode from 'vscode';
 import * as cheerio from 'cheerio';
 
-export class Extension {
-  constructor(private readonly context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.htmlFixer', function () {
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            let document = editor.document;
+            let text = document.getText();
 
-  activate() {
-    let disposable = vscode.commands.registerCommand('extension.htmlFixer', this.htmlFixer);
+            let $ = cheerio.load(text);
+            let fixedHtml = $.html();
+            try {
+              $ = cheerio.load(text);
+              fixedHtml = $.html();
+            } catch (error) {
+              console.error('Error parsing HTML:', error);
+              vscode.window.showErrorMessage('Failed to parse HTML.');
+              return;
+            }
 
-    this.context.subscriptions.push(disposable);
-  }
+            editor.edit(editBuilder => {
+                let firstLine = document.lineAt(0);
+                let lastLine = document.lineAt(document.lineCount - 1);
+                let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
 
-  htmlFixer() {
-    let editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
+                // Replace the text in the editor.       
+                editBuilder.replace(textRange, fixedHtml);
 
-    let document = editor.document;
-    let text = document.getText();
-
-    let $;
-    let fixedHtml: string;
-    try {
-      $ = cheerio.load(text);
-      fixedHtml = $.html();
-    } catch (error) {
-      console.error('Error parsing HTML:', error);
-      vscode.window.showErrorMessage('Failed to parse HTML.');
-      return;
-    }
-
-    editor.edit(editBuilder => {
-      let firstLine = document.lineAt(0);
-      let lastLine = document.lineAt(document.lineCount - 1);
-      let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-
-      // Replace the text in the editor.
-      editBuilder.replace(textRange, fixedHtml);
-
-      // Check if the text was successfully replaced.
-      let originalText = document.getText(textRange);
-      if (originalText !== fixedHtml) {
-        vscode.window.showInformationMessage('HTML fixing complete!');
-      } else {
-        vscode.window.showErrorMessage('Failed to apply edit.');
-      }
+                // Check if the text was successfully replaced.
+                let originalText = document.getText(textRange);
+                if (originalText !== fixedHtml) {
+                  vscode.window.showInformationMessage('HTML fixing complete!');
+                } else {
+                  vscode.window.showErrorMessage('Failed to apply edit.');
+                }
+            });
+        }
     });
-  }
+
+    context.subscriptions.push(disposable);
 }
+
+
+
+export function deactivate() {}
